cd22a0fa8e0c9a827341904da2a0877ee83591ee
eddyerburgh <edward.yerburgh@gmail.com>
2019-09-02 21:20:49 +0100

    refactor: append single string, instead of multiple

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 function ReactDevToolsIFrameInjectPlugin(options) {}
 
-const str = [
-  "if (window.parent !== window) {\n",
-  "window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = window.parent.__REACT_DEVTOOLS_GLOBAL_HOOK__;\n",
-  "}\n"
-];
+const str =
+  "if (window.parent !== window) {\n" +
+  "  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = window.parent.__REACT_DEVTOOLS_GLOBAL_HOOK__;\n" +
+  "}\n";
 
 ReactDevToolsIFrameInjectPlugin.prototype.apply = function(compiler) {
   compiler.hooks.emit.tap("ReactDevToolsIframe", function(compilation) {
-    for (i = str.length - 1; i >= 0; i--) {
-      Object.keys(compilation.assets).forEach(key => {
-        if (!key.match(/.js$/)) {
-          return;
-        }
-        if (compilation.assets[key]._source) {
-          compilation.assets[key]._source.children.unshift(str[i]);
-        }
-      });
-    }
+    Object.keys(compilation.assets).forEach(key => {
+      if (!key.match(/.js$/)) {
+        return;
+      }
+      if (compilation.assets[key]._source) {
+        compilation.assets[key]._source.children.unshift(str);
+      }
+    });
   });
 };
 
